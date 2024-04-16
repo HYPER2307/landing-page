cdc108f4454c0b31ceb7484b79f006b240ee7a6b
Adrian Yevchuk <adrintik@gmail.com>
2024-04-16 14:49:45 +0300

    make build

---
+++ index.d889be15.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".form"),n=document.querySelectorAll(".form__input"),t=function(){n.forEach(function(e){e.value=""})};e.addEventListener("submit",function(e){e.preventDefault(),t()});var o=document.querySelector(".page__body");window.addEventListener("hashchange",function(){"#menu"===window.location.hash?o.classList.add("page__body--with-menu"):o.classList.remove("page__body--with-menu")});
+//# sourceMappingURL=index.d889be15.js.map
